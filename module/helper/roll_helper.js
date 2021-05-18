88eb4fac5cb01da46ee56755ca36be7a46fbb660
jsavko <james@geekwebservices.com>
2021-05-18 10:44:21 -0700

    Updated Defense Rolls
    Updated to 0.8.4 and updated Defense rolls from the d20 icon

--- module/helper/roll_helper.js
+++ module/helper/roll_helper.js
@@ -21,6 +21,10 @@ export class RollHelper {
                     <option value="-3">-3</option>
                 </select>
             </div>
+                <div class="form-group">
+                <label>Defence Roll?</label>
+                <input type="checkbox" id="rolltype-defense" name="rolltype-defense" value="1"> Yes
+            </div>
             </form>
             `,
             buttons: {
@@ -39,30 +43,70 @@ export class RollHelper {
             close: html => {
                 if (confirmed) {
                     let rollModifier = parseInt(html.find('[name=modifier-value]')[0].value);
+                    let rollisDefense= html.find(`[name=rolltype-defense]`).is(":checked");
+                    let token = canvas.tokens.controlled;
+                     //console.log(token);
+                   if (token.length == 0) {
+                        ui.notifications.error("You must have an actor to roll a defense roll");
+                   } else {
+                       
+                   }
+
                     if (rollModifier >= 0) { 
-                        var roll = new Roll("2d6 +" + rollModifier);
+                        var roll = new Roll("2d6 +" + rollModifier, token[0].actor.data);
                     } else {
-                        var roll = new Roll("2d6 " + rollModifier);
+                        var roll = new Roll("2d6 " + rollModifier, token[0].actor.data);
                     }
                     roll.evaluate({async: true}).then(
                         function(result) {
                             console.log(result); // "Stuff worked!"
-
-                            var RollResult = {type: "action", outcome:"Complete Success", apptitude: rollModifier, roll: result };
-                            if (result._total < 7) {
-                                RollResult.outcome = "Failure";
-                              } else if (result._total < 10) {
-                                RollResult.outcome = "Partial Success";
-                              } else if (result._total > 12) {
-                                RollResult.outcome = "Critical Success";
-                              }
-                            let template = 'systems/vagabonds/templates/chat/rolls.html';
-                            var RollTemplate = renderTemplate(template, RollResult).then(content => {
-                                result.toMessage({
-                                speaker: ChatMessage.getSpeaker({ }),//actor: this.actor }),
-                                flavor: content,
-                              });
-                            });
+                            //Set Roll Type
+                            if (rollisDefense == true) {
+                                //Making a defense roll
+                                var RollResult = {type: "defend", high: "0", low:"0", damage:"No", outcome:" Outright success", roll: roll };
+                                if (result.terms[0].results[0].result > result.terms[0].results[1].result) {
+                                  RollResult.high = result.terms[0].results[0].result;
+                                  RollResult.low = result.terms[0].results[1].result;
+                                } else {
+                                  RollResult.low = result.terms[0].results[0].result;
+                                  RollResult.high = result.terms[0].results[1].result;
+                                }
+                      
+                                if (result._total < 7) {
+                                  RollResult.outcome = "Failure";
+                                  RollResult.damage =  (RollResult.high - result.data.data.armor.value);
+                                } else if (result._total < 10) {
+                                  RollResult.outcome = "Partial Success";
+                                  RollResult.damage = (RollResult.low - result.data.data.armor.value)
+                                } 
+                      
+                                let template = 'systems/vagabonds/templates/chat/rolls.html';
+                                var RollTemplate = renderTemplate(template, RollResult).then(content => {
+                                  result.toMessage({
+                                    speaker: ChatMessage.getSpeaker({actor: result.data}),
+                                    flavor: content,
+                                  });
+                                });
+                                
+                            } else {
+                                //Normal Roll
+                                var RollResult = {type: "action", outcome:"Complete Success", apptitude: rollModifier, roll: result };
+                                if (result._total < 7) {
+                                    RollResult.outcome = "Failure";
+                                  } else if (result._total < 10) {
+                                    RollResult.outcome = "Partial Success";
+                                  } else if (result._total > 12) {
+                                    RollResult.outcome = "Critical Success";
+                                  }
+                                let template = 'systems/vagabonds/templates/chat/rolls.html';
+                                var RollTemplate = renderTemplate(template, RollResult).then(content => {
+                                    result.toMessage({
+                                    speaker: ChatMessage.getSpeaker({ }),//actor: this.actor }),
+                                    flavor: content,
+                                  });
+                                });
+                            
+                            }
 
                         }
 
